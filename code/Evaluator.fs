5662eb3251058557f6b32fdbbfc6b7d24e1e13a7
Tim <tef1@williams.edu>
2023-11-30 14:06:06 -0500

    addedCounter

--- code/Evaluator.fs
+++ code/Evaluator.fs
@@ -27,8 +27,34 @@ let evalScheme(schm: string) =
     <line x1=\"850\" y1=\"395\" x2=\"640\" y2=\"290\" stroke=\"black\" stroke-width=\"3\"/>\n
     <line x1=\"630\" y1=\"305\" x2=\"650\" y2=\"275\" stroke=\"black\" stroke-width=\"3\"/>\n"
 
+
+    let ctr = "<!--Counter Scheme against 3-4 Defense-->\n
+    <!--Labeling Keys-->\n
+    <text x=\"417\" y=\"285\" font-size=\"30\" stroke=\"red\" font-family=\"Arial, Helvetica, sans-serif\">K</text>\n
+    <text x=\"597\" y=\"235\" font-size=\"30\" stroke=\"red\" font-family=\"Arial, Helvetica, sans-serif\">K</text>\n
+    <!--Pull for -1-->\n
+    <line x1=\"530\" y1=\"455\" x2=\"545\" y2=\"495\" stroke=\"black\" stroke-width=\"3\"/>\n
+    <line x1=\"545\" y1=\"495\" x2=\"790\" y2=\"495\" stroke=\"black\" stroke-width=\"3\"/>\n
+    <line x1=\"790\" y1=\"495\" x2=\"790\" y2=\"300\" stroke=\"black\" stroke-width=\"3\"/>\n
+    <line x1=\"770\" y1=\"300\" x2=\"810\" y2=\"300\" stroke=\"black\" stroke-width=\"3\"/>\n
+    <!--Kickout-->\n
+    <line x1=\"610\" y1=\"455\" x2=\"610\" y2=\"485\" stroke=\"black\" stroke-width=\"3\"/>\n
+    <line x1=\"610\" y1=\"485\" x2=\"870\" y2=\"480\" stroke=\"black\" stroke-width=\"3\"/>\n
+    <line x1=\"870\" y1=\"460\" x2=\"870\" y2=\"500\" stroke=\"black\" stroke-width=\"3\"/>\n
+    <!--Down Block on Frank-->\n
+    <line x1=\"690\" y1=\"395\" x2=\"560\" y2=\"375\" stroke=\"black\" stroke-width=\"3\"/>\n
+    <line x1=\"555\" y1=\"390\" x2=\"565\" y2=\"355\" stroke=\"black\" stroke-width=\"3\"/>\n
+    <!--Down Block on Nose-->\n
+    <line x1=\"770\" y1=\"395\" x2=\"730\" y2=\"375\" stroke=\"black\" stroke-width=\"3\"/>\n
+    <line x1=\"725\" y1=\"390\" x2=\"735\" y2=\"355\" stroke=\"black\" stroke-width=\"3\"/>\n
+    <!--Block to BS Key-->\n
+    <line x1=\"850\" y1=\"395\" x2=\"640\" y2=\"290\" stroke=\"black\" stroke-width=\"3\"/>\n
+    <line x1=\"630\" y1=\"305\" x2=\"650\" y2=\"275\" stroke=\"black\" stroke-width=\"3\"/>\n
+"
+
     match schm with
     | "power" -> pwr
+    | "counter" -> ctr
     | _ -> ""
 
 let evalDefense (cov: string, box: string) =
