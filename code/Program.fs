d832d90b528bbe158bb7e34c58ebba880ff54a4f
Tim <tef1@williams.edu>
2023-12-10 20:01:11 -0500

    addedstuff

--- code/Program.fs
+++ code/Program.fs
@@ -8,9 +8,10 @@ let main args =
     let text = IO.File.ReadAllText file
     match parse text with
     | Some ast ->
-        let svg = eval ast
+        let svg = evalPlaybook ast
         printfn "%s" svg
         0
     | None ->
+        printfn "%s" text
         printfn "Invalid program."
         1
