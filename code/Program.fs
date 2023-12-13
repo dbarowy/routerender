447acc0dccfb501dc4fbb405870a4e0c52102f95
ericgage-williams <ekg3@williams.edu>
2023-12-13 11:23:01 -0500

    updated program

--- code/Program.fs
+++ code/Program.fs
@@ -1,15 +1,17 @@
 ﻿open Evaluator
-open System
+open System.IO
 open Parser
 
 [<EntryPoint>]
 let main args =
     let file = args[0]
-    let text = IO.File.ReadAllText file
+    let text = File.ReadAllText file
     match parse text with
     | Some ast ->
         let svg = eval ast
-        printfn "%s" svg
+        let path = __SOURCE_DIRECTORY__
+        let fullPath = Path.Combine (path, Path.ChangeExtension (file, "svg"))
+        File.WriteAllText (fullPath, svg)
         0
     | None ->
         printfn "Invalid program."
